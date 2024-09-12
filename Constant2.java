80aeb76963a86bf375e8613087060d894f462471
Aadhya9079 <146864352+Aadhya9079@users.noreply.github.com>
2024-09-12 15:41:11 +0530

    Add files via upload

---
+++ Constant2.java
@@ -0,0 +1,7 @@
+public class Constant2 {
+    static String AvailableOperations = (" Available Operations:\n Task 1 : Count Palindromes \n Task 2 : Nth Fibonacci \n Task 3 : Snake To Camel Case \n Task 4 : Count Consonants \n Task 5 : Binary To Decimal");
+    static String InputMessage = "Enter the index for fibonacci :";
+    static String InvalidMessage = "Invalid operation. Please choose from the given task.";
+    static String ExecutionMessage = "What would you like to execute:";
+    static String YesOrNoMessage = "Do you want to perform another operation? (yes/no): ";
+}
